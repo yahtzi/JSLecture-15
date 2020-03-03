7859b8b429679ae731a23bb7fa874369c44aba71
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-03-02 20:46:40 -0800

    I did parts 2-5 but Iforgot to commit in between

--- scprit.js
+++ scprit.js
@@ -1,5 +1,9 @@
 let submitButton = document.getElementById("submitButton");
+let textInput = document.getElementById("textInput");
+let colorInput = document.getElementById("colorInput");
 
 function submitButton(event) {
     event.preventDefault();
+    console.log(textInput);
+    console.log(colorInput);
 }
